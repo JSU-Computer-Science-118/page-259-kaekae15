09f64525abfac9bc8bb443c0ca554a4e949cb71a
kaekae15 <117469426+kaekae15@users.noreply.github.com>
2022-11-14 11:57:54 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,17 +11,17 @@ int main(){
     double a,b,c,discriminant;
     discriminant = pow(b,2.0)-4*a*c;
   
-    if (discriminant > 0);{
+    if (discriminant > 0){
         cout << "Two real roots" << endl;
         cout << "- First root " << (-b + pow(discriminant, 1.0/2.0))/(2.0*a) << endl;
         cout << "- Second root " << (-b-pow(discriminant,1.0/2.0))/(2.0*a) <<endl;
     }
    
-    if (discriminant < 0);{
+    if (discriminant < 0){
         cout << "Complex roots" << endl;
     }
    
-    if (discriminant==0);{
+    if (discriminant==0){
         cout << "The single repeated root is " << -b/(2.0*a) << endl; 
     }
     return 0;
