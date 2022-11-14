ecb1c7d9a10c8e30a60827246ecf7f6124afbf55
kaekae15 <117469426+kaekae15@users.noreply.github.com>
2022-11-14 13:06:21 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,8 +17,8 @@ int main(){
     }
     else if (discriminant > 0){  // Ouput: Two real roots \n- First root 0\n- Second root -1
         cout << "Two real roots" << endl;
-        cout << "- First root " << (-b + pow(discriminant, 1.0/2.0))/(2.0*a) << endl 
-             << "- Second root " << (-b - pow(discriminant,1.0/2.0))/(2.0*a) <<endl;
+        cout << "- First root " << (-b + sqrt(discriminant)/(2.0*a)) << endl ;
+             << "- Second root " << (-b - sqrt(discriminant)/(2.0*a)) <<endl;
     }
     else if (discriminant < 0) // Output: Complex roots
         cout << "Complex roots" << endl;
