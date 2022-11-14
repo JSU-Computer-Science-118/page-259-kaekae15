086661b6412cca60210e1c09a2e7155dd06ec4e6
kaekae15 <117469426+kaekae15@users.noreply.github.com>
2022-11-14 13:18:37 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,10 @@ int main(){
     cin >> a >> b >> c;
     discriminant = pow(b,2.0)-4*a*c;
 
-    if (discriminant==0){
+    if (a==0){
+        cout << "Invalid entry";
+    }
+    else if (discriminant==0){
         cout << "The single root is " << -b/(2.0*a) << endl; 
     }
     else if (discriminant > 0){  // Ouput: Two real roots \n- First root 0\n- Second root -1
@@ -23,8 +26,5 @@ int main(){
     else if (discriminant < 0){ // Output: Complex roots
         cout << "Complex roots" << endl;
     }
-    else {
-        cout << "Invalid entry";
-        }
     return 0;
 }
